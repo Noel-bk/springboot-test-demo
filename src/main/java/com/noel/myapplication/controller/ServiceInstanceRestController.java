abf85c4f637f26a638edc5bed80ad41321f792e2
Jaeyeong Baek <jaeyeong.b@gmail.com>
2019-05-15 00:07:17 +0900

    Configure discovery client

---
+++ src/main/java/com/noel/myapplication/controller/ServiceInstanceRestController.java
@@ -0,0 +1,24 @@
+package com.noel.myapplication.controller;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ServiceInstanceRestController {
+
+    private final DiscoveryClient discoveryClient;
+
+    public ServiceInstanceRestController(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
+    @GetMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+}
